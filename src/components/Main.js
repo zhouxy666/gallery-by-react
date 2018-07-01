cb7384e3be7effa33fb8461bc7659ce1a348c78d
zhouxy <zhouxy_123@126.com>
2018-07-01 14:56:05 +0800

    test

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ class AppComponent extends React.Component {
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <span>Hello,world234!</span>
+        <span>Hello,world!</span>
       </div>
     );
   }
